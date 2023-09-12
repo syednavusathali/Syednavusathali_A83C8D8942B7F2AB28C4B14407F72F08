f6d9452b31c143758a56e7271e0886e4aca6273d
syednavusathali <144645396+syednavusathali@users.noreply.github.com>
2023-09-12 13:34:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def fact(n):
+  if (n==0):
+    return 1
+  else:
+    return (n*fact(n-1))
+  n=input("enter the value of n:")
+  print("the factorical of",n,"is:",fact(n))                        Online Python Compiler.
